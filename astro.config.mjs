1e6d84462342e0d95c590b1789b07a0e05b6573d
jrbeast1998 <jrbeast1998@gmail.com>
2026-06-23 17:58:09 -0400

    Standardize on www domain, resolve trailing slash redirects, and optimize PPF meta description

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://royaltyautocare.net',
+  site: 'https://www.royaltyautocare.net',
   integrations: [sitemap()],
 });
